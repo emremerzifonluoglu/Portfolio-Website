fbaec30b9ebdac38073432fdd0751247b1a2870b
Emre <emremer91@yahoo.com.tr>
2022-09-01 17:01:02 +0300

    added project

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 export const products = [
     {
         id: 1,
-        img: "https://picsum.photos/id/57/400/300",
-        link: "https://picsum.photos/id/57/400/300",
+        img: "https://raw.githubusercontent.com/emremerzifonluoglu/portfolio-website/main/src/img/portfolio-website.jpg",
+        link: "https://emremerzifonluglu.netlify.app/",
     },
     {
         id: 2,
-        img: "https://picsum.photos/id/67/400/300",
+        img: "https://raw.githubusercontent.com/emremerzifonluoglu/portfolio-website/main/src/img/todos-list.jpg",
         link: "https://picsum.photos/id/67/400/300",
     },
     {
         id: 3,
-        img: "https://picsum.photos/id/77/400/300",
+        img: "https://raw.githubusercontent.com/emremerzifonluoglu/react-weather-app/master/react-weather-app.jpg",
         link: "https://picsum.photos/id/77/400/300",
     },
     {
