4bfc7c1b248c015f97b6443b98938d001333aba8
EMRE MERZİFONLUOĞLU <emremer91@yahoo.com.tr>
2022-10-12 12:46:00 +0300

    add project

--- src/data.js
+++ src/data.js
@@ -6,28 +6,31 @@ export const products = [
     },
     {
         id: 2,
+        img: "https://github.com/emremerzifonluoglu/noxart-clon/blob/version-v2/src/img/noxart-clon.jpg?raw=true",
+        link: "https://github.com/emremerzifonluoglu/noxart-clon",
+
+    },
+    {
+        id: 3,
         img: "https://raw.githubusercontent.com/emremerzifonluoglu/ozer-proje/master/Ozer-proje.jpg?token=GHSAT0AAAAAABRWRR5BA4M6CRJZBM7CH7UEYZEUV7Q",
         link: "https://ozerproje.netlify.app/Home",
 
     },
 
+
     {
-        id: 3,
+        id: 4,
         img: "https://raw.githubusercontent.com/emremerzifonluoglu/Asian-Kiychen-Menu/main/asian-kitchen's-menu.jpg",
         link: "https://asiankitchenmenu.netlify.app/",
 
     },
     {
-        id: 4,
+        id: 5,
         img: "https://raw.githubusercontent.com/emremerzifonluoglu/Bootstrap-EMmarket/main/em-market.jpg",
         link: "https://em-market.netlify.app/",
     },
-    {
-        id: 5,
-        img: "https://raw.githubusercontent.com/emremerzifonluoglu/react-weather-app/master/react-weather-app.jpg",
-        link: "https://github.com/emremerzifonluoglu/react-weather-app",
 
-    },
+
     {
         id: 6,
         img: "https://raw.githubusercontent.com/emremerzifonluoglu/portfolio-website/main/src/img/todos-list.jpg",
